6631208d704c31fab3f05f182b3939c8d788c05b
aymane lotfi <31337952+aymane-lotfi@users.noreply.github.com>
2018-03-25 17:43:41 +0100

    Add files via upload

---
+++ algo-euclide.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+class Algo Euclide {
+	public static void main (String[] args){
+	   Scanner sc = new Scanner(System.in);
+	    System.out.println("- Calcul du PGCD -  made by 3 Lotfi Aymane ");
+	    System.out.println("A=");
+	    int a= sc.nextInt();
+	    System.out.println("B=");
+	    int b = sc.nextInt();
+	 @override
+	    System.out.println();
+	 
+	    final int nb1 = a;
+	    final int nb2 = b;
+	 
+	    if ( a < b ){
+	        a = nb2;
+	        b = nb1;
+	    }
+	    int r = a%b;
+	    int result = b;
+	    while ( r !=0 ) {
+	        result = r;
+	        r = a%b;
+	        a=b;
+	        b=r;
+	    }
+	    System.out.println(a + " = " + b + " * " + (a/b) + " + " + r);
+	    System.out.print("PGCD(" + nb1 + ", " + nb2 + ") = " +result);
+	}}
